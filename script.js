09d49780f3869376d528f9bb917665eb85853497
porsii800 porsi800 <Gporsi870@gmail.com>
2024-06-06 13:21:03 +0000

    adicianada a funcao para escolher o tema oo quiz no index.html

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ const body = document.querySelector("body")
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
+verificarTema(body, botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao =>{
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
